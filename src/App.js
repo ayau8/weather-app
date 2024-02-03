8b8623d17766d5b60d57a11c2531416b54cc0eae
ayau8 <ayaualvin@gmail.com>
2024-02-03 20:52:24 +0900

    add open weather api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Input } from 'postcss';
+import { useState, useEffect } from 'react';
 import './App.css';
 import TopButtons from './components/TopButtons';
 import Inputs from './components/Inputs';
@@ -6,8 +6,30 @@ import TimeAndLocation from './components/TimeAndLocation';
 import TemperatureAndDetails from './components/TemperatureAndDetails';
 import Forecasts from './components/Forecasts';
 
-
 function App() {
+  const [data, setData] = useState(null);
+  const [countries, setCountries] = useState("Tokyo")
+
+  const TESTING_KEY = process.env.REACT_APP_TESTING_KEY
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${countries}&appid=${TESTING_KEY}`)
+        if (!response.ok) {
+          throw new Error('Network response was not ok.')
+        } else {
+          const data = await response.json()
+          console.log(data)
+          setData(data.articles)
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchData()
+  }, [countries])
+
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadown-xl shadow-gray-400">
         <TopButtons />
