6318bffbf8555f341eef03d4ff5ca1097c570263
ayau8 <ayaualvin@gmail.com>
2024-02-07 00:21:48 +0900

    weatherService

---
+++ src/services/weatherService.js
@@ -0,0 +1,71 @@
+import { DateTime } from "luxon";
+
+const API_KEY = 'c338eb81fead5a23d8fd7f80814f046f';
+const BASE_URL = "https://api.openweathermap.org/data/2.5/";
+
+const getWeatherData =  (infoType, searchParams) => {
+  const url = new URL(BASE_URL + "/" + infoType);
+  url.search = new URLSearchParams({...searchParams, appid: API_KEY});
+  return fetch(url).then((res) => res.json())
+};
+
+const formatCurrentWeather = (data) => {
+  // destructing
+  const {
+    coord: {lat, lon},
+    main: {temp, feels_like, temp_max, temp_min, humidity},
+    name,
+    dt,
+    timezone,
+    sys: {country, sunrise, sunset},
+    weather,
+    wind: {speed}
+  } = data
+
+  const {main: details, icon} = weather[0]
+
+  return {lat, lon, temp, feels_like, temp_max, temp_min, humidity, name, dt, country, sunrise, sunset, details, icon, speed, timezone}
+}
+
+const formatForecastWeather = (data) => {
+  let { list } = data;
+
+  list = list.slice(3, 8).map(d => {
+    return {
+      title: d.dt_txt,
+      temp: Math.round(d.main.temp),
+      icon: d.weather[0].icon
+    }
+  });
+
+    return { list };
+}
+
+const getFormattedWeatherData = async searchParams => {
+  const formattedCurrentWeather = await getWeatherData(
+      'weather', 
+      searchParams
+    ).then(formatCurrentWeather);
+
+    const {lat, lon} = formattedCurrentWeather
+
+    const formattedForecastWeather = await getWeatherData(
+      'forecast', {
+      lat, 
+      lon, 
+      units: 'metric',
+    }).then(formatForecastWeather)
+
+  return {...formattedCurrentWeather, ...formattedForecastWeather};
+};
+
+const dateTimetoLocalTime = (timezone) => DateTime.now().setZone(`UTC${timezone < 0 ? '' : '+'}${ timezone / 3600 }`).toFormat("cccc, dd LLL yyyy' | Local time:' hh:mm a");
+
+const dateTimeObject = time => DateTime.fromFormat(time, 'yyyy-MM-dd HH:mm:ss').toFormat('HH:mm');
+
+const iconUrlFromCode = code => `http://openweathermap.org/img/wn/${code}@2x.png`
+
+export default getFormattedWeatherData;
+
+export { dateTimeObject, iconUrlFromCode, dateTimetoLocalTime };
+
